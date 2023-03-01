7f0d33d79a8e29c973f291652141c3377336b3b8
Hareem Khan <hareem.khan@ucalgary.ca>
2023-02-28 22:44:46 -0700

    added menu bar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
 function App() {
-  return <h1>Lotion</h1>;
+  return (
+    <div>
+      <div class="header"> 
+        <h1>Lotion</h1> 
+        <p>Like Notion, but worse.</p>
+
+        <button class="button"
+          type="button">&#9776;
+        </button>
+      
+      </div>
+      
+      <div class="navbar">
+        
+      </div>
+
+
+    </div>
+    
+  );
+
 }
 
 export default App;
