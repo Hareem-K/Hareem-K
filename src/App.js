97ca4f143ba4cd4cceff1634c4bd2500d03ef867
Hareem Khan <hareem.khan@ucalgary.ca>
2023-03-01 18:53:41 -0700

    added a note demo to sidebar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import Header from "./Header";
+import Sidebar from "./Sidebar";
 
 function App() {
   return(
     <>
     <Header />
+    <Sidebar />
     
     </>
   )
---
+++ src/Sidebar.js
@@ -0,0 +1,27 @@
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            <div className="subheader"> 
+                <h2>Notes</h2>
+                <button> &#x2b; </button>
+
+            </div>
+
+            <div className="sidebarNotes">
+                <div className="sidebarnote">
+                    <div className="notetitle">
+
+                        <strong>Title</strong>
+                    </div>
+                    <small className="note-mod-info">Last modified [date]</small>
+                    <div className="preview"> Note Preview</div>
+                </div>
+            </div>
+
+        </div>
+
+
+    )
+}
+
+export default Sidebar;
