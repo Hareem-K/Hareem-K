37a571b1da129354fe84b925efb916a22f32b8af
Hareem Khan <hareem.khan@ucalgary.ca>
2023-03-01 17:22:57 -0700

    need to fix header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
+import React from "react";
+import Header from "./Header";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+
 function App() {
-  return (
-    <div>
-      <div class="header"> 
-        <h1>Lotion</h1> 
-        <p>Like Notion, but worse.</p>
-
-        <button class="button"
-          type="button">&#9776;
-        </button>
-      
-      </div>
-      
-      <div class="navbar">
-        
-      </div>
-
-
-    </div>
-    
-  );
-
+  return(
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Header />}></Route>
+      </Routes>
+    </BrowserRouter>
+  )
 }
+
 
 export default App;
---
+++ src/Header.js
@@ -0,0 +1,14 @@
+function Header() {
+    return (
+        <div className="header"> 
+            <h1>Lotion</h1> 
+            <p>Like Notion, but worse.</p>
+            
+            <button className="button"
+                type="button">&#9776;
+            </button>  
+        </div>
+    )
+}
+
+export default Header; 
