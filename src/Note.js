1727a38f2d4536038f5cc588f29310f108961fd2
Hareem Khan <hareem.khan@ucalgary.ca>
2023-03-07 02:26:20 -0700

    start/edit buttons working, need to add calendar and routes

--- src/Note.js
+++ src/Note.js
@@ -1,8 +1,27 @@
+//import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+
 function Note({isOpen, activeNote, onDeleteNote, onUpdateNote}) {
     const noteStyle = {
         marginLeft: isOpen ? 350 : 0
     }
 
+    const [editing, setEditing] = useState(false);
+    const [noteContent, setNoteContent] = useState(activeNote?.body || "");
+
+    const handleEdit = () => {
+        setEditing(true);
+    };
+
+    const handleSaveNote = () => {
+        const strippedContent = noteContent.replace(/<[^>]+>/g, ''); // Remove HTML tags
+        onEditField("body", strippedContent);
+        setEditing(false);
+    };
+
+    //brings most recently edited note to the top of sidebar
     const onEditField = (key, value) => {
         onUpdateNote({
             ...activeNote,
@@ -11,39 +30,41 @@ function Note({isOpen, activeNote, onDeleteNote, onUpdateNote}) {
         });
     };
 
-    if(!activeNote) return <div className="no-active-note">Select a note, or create a new one.</div>
+    //message if no current note selected
+    if(!activeNote) return <div className="no-active-note">Select a note, or create a new one.</div>;
 
     return (
         <div className="Note" style={noteStyle}> 
             <div className="app-main-note-edit">
-                <input type="text" id="title" value={activeNote.title} onChange={(e) => onEditField("title", e.target.value)} autoFocus/>
-                <p className="date">{new Date(activeNote.lastModified).toLocaleDateString("en-CA",{
-                                hour: "2-digit",
-                                minute: "2-digit",
-                            } )}
-                </p>
-
+                <input style={{marginLeft: -25}} type="text" id="title" value={activeNote.title} onChange={(e) => {
+                    if (editing) {
+                        onEditField("title", e.target.value)
+                    }
+                }} disabled={!editing} autoFocus={!editing}/>
+                <p className="date">{new Date(activeNote.lastModified).toLocaleDateString("en-CA", {
+                    hour: "2-digit",
+                    minute: "2-digit",
+                })}</p>
                 <div className="buttons">
-                    <button className="save">Edit</button>
+                    {editing ? (
+                        <button className="save" onClick={handleSaveNote}>Save</button>
+                    ) : (
+                        <button className="edit" onClick={handleEdit}>Edit</button>
+                    )}
                     <button onClick={() => onDeleteNote(activeNote.id)} className="delete">delete</button>
                 </div>
-                <textarea id="body" placeholder="Your Note Here" value={activeNote.body} onChange={(e) => onEditField("body", e.target.value)}/> 
-            </div>
-
-            <div className="app-main-note-preview">
-                <h1 className="preview-title">{activeNote.title}</h1>
-                <p className="date">{new Date(activeNote.lastModified).toLocaleDateString("en-CA",{
-                                hour: "2-digit",
-                                minute: "2-digit",
-                            } )}
-                </p>
-                <div className="markdown-preview">
-                    {activeNote.body}
+                <div className="noteBody">
+                    {editing ? (
+                        <ReactQuill style={{height: 525, marginLeft: -25}} className="no-outline" id="body" placeholder="Your Note Here" value={noteContent} onChange={setNoteContent} />
+                    ) : (
+                        <div className="markdown-preview">
+                            {activeNote.body}
+                        </div>
+                    )}
                 </div>
             </div>
-            
         </div>
-    )
+    );
 }
 
 export default Note;
